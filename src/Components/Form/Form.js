a53eda8ca687f8987317ef414b0095f43ed254e5
aimunir <aimunir25@outlook.com>
2022-06-09 17:34:21 +0600

    form added

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div className='form'>
+        <form action="">
+            <input type="text" placeholder='Enter your email' /> <br />
+             <input type="password" placeholder='Enter your password'/> <br />
+             <button>Submit</button>
+        </form>
+
+
+    </div>
+  )
+}
---
+++ src/Firebaseconfig.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALwJXHbRY2bB92Io2SjAp3H0hf7ttfFi0",
+  authDomain: "react-form-ecd8f.firebaseapp.com",
+  projectId: "react-form-ecd8f",
+  storageBucket: "react-form-ecd8f.appspot.com",
+  messagingSenderId: "209952806363",
+  appId: "1:209952806363:web:f1b3e91e4634ec147c03fe"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
